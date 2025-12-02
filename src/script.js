10a829dd4dd8d147d7f453189bd64728b577982b
Aciel Olivera <acielito64@gmail.com>
2025-12-02 07:11:41 +0100

    Cambios en los estilos, añadida seccion de trabajadores responsive, seccion de peliculas, musica y series

---
+++ src/script.js
@@ -0,0 +1,19 @@
+let slideIndex = 0;
+
+function moveSlide(step) {
+  const slides = document.querySelector('.card-container');
+  const totalSlides = document.querySelectorAll('.card').length;
+
+  slideIndex += step;
+
+  // Asegurarse de que el índice de la diapositiva esté dentro del rango
+  if (slideIndex < 0) {
+    slideIndex = totalSlides - 1;
+  } else if (slideIndex >= totalSlides) {
+    slideIndex = 0;
+  }
+
+  // Mover el contenedor de las tarjetas
+  const slideWidth = document.querySelector('.card').offsetWidth;
+  slides.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+}
